9203bda2d4eb7871912cb1d28a1b38453ec869a7
denispashin <dpashin@gmail.com>
2022-11-16 14:33:05 +0500

    Initial commit

---
+++ src/test/java/ru/webred/mypain/MypainApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.webred.mypain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MypainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
